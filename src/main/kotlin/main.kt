09e9ae427a11b449c324af24fbb06af9bf0608e1
Juliana Lima <julianalimatsi@hotmail.com>
2022-07-09 01:46:40 -0300

    ByteBanco parte 1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,36 @@
-fun main(){
-    print("Olá Mundo")
+fun main() {
+    println("Bem Vindo ao ByteBanco")
+    var conta = Conta()
+    println()
+
+}
+
+class Conta {
+    var titular = ""
+    var numero = 8001
+    var saldo = 9000
+}
+
+fun testaLassos() {
+    val titular: String = "Pedro Lima"
+    val numeroConta: Int = 8000
+    var saldo = 8000
+
+    println("titular $titular")
+    println("numero da conta $numeroConta")
+    println("saldo da conta $saldo")
+
+}
+
+fun testaSaldo(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+
+    } else {
+        if (saldo == 0.0) {
+            println("conta é neutra")
+        } else {
+            println("conta é negativa")
+        }
+    }
 }
