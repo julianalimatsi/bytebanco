dda38d1ea7c9dc24979b4499d1aaa5681b365a46
Juliana Lima <julianalimatsi@hotmail.com>
2022-07-10 03:51:53 -0300

    Implementando depósito parte 1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,35 @@
 fun main() {
     println("Bem Vindo ao ByteBanco")
-    var conta = Conta()
-    println()
+    val contaJuliana = Conta()
+    contaJuliana.titular = "Juliana"
+    contaJuliana.numero = 9000
+    contaJuliana.saldo = 900
+
+    val contaPedro = Conta()
+    contaPedro.titular = "Pedro"
+    contaPedro.numero = 8000
+    contaPedro.saldo = 800
+
+    println(contaJuliana.titular)
+    println(contaJuliana.numero)
+    println(contaJuliana.saldo)
+
+    println(contaPedro.titular)
+    println(contaPedro.numero)
+    println(contaPedro.saldo)
+
+    println("depósito realizado conta do Pedro")
+    deposita(contaPedro, valor = 90.0)
+    println(contaPedro.saldo)
+
+    println("depósito realizado conta do Juliana")
+    println(contaJuliana.saldo)
+    deposita(contaJuliana, valor = 99.0)
+
+}
+
+fun deposita(conta: Conta, valor: Double) {
+    conta.saldo +=
 
 }
 
@@ -9,6 +37,27 @@ class Conta {
     var titular = ""
     var numero = 8001
     var saldo = 9000
+}
+
+fun testaReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+    var contaMaria = Conta()
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("titular conta João ${contaJoao.titular}")
+    println("titular conta Maria ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
 }
 
 fun testaLassos() {
