6cff5f01cbd46dfb63a6221d542fd10356b0b62b
Juliana Lima <julianalimatsi@hotmail.com>
2022-08-05 00:55:07 -0300

    Implementando tranferÊncia parte 1.

---
+++ src/main/kotlin/Conta.kt
@@ -0,0 +1,32 @@
+class Conta {
+
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+        set(valor) {
+            if (valor > 0)
+            field = valor
+        }
+
+    fun deposita(valor: Double) {
+        if (valor > 0) {
+            this.saldo += valor
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.deposita(valor)
+            return true
+        }
+        return false
+    }
+
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            saldo -= valor
+        }
+    }
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,12 +3,12 @@ fun main() {
     val contaJuliana = Conta()
     contaJuliana.titular = "Juliana"
     contaJuliana.numero = 9000
-    contaJuliana.saldo = 900.0
+    contaJuliana.saldo = -200.0
 
     val contaPedro = Conta()
     contaPedro.titular = "Pedro"
     contaPedro.numero = 8000
-    contaPedro.saldo = 800.0
+    contaPedro.saldo = 200.0
 
     println(contaJuliana.titular)
     println(contaJuliana.numero)
@@ -18,101 +18,86 @@ fun main() {
     println(contaPedro.numero)
     println(contaPedro.saldo)
 
-    println("depósito realizado conta do Pedro")
-    contaPedro.deposita(valor = 90.0)
-    println(contaPedro.saldo)
+//    println("depósito realizado conta do Pedro")
+//    contaPedro.deposita(valor = 90.0)
+//    println(contaPedro.saldo)
+//
+//    println("depósito realizado conta do Juliana")
+//    println(contaJuliana.saldo)
+//    contaJuliana.deposita(valor = 99.0)
+//
+//    println("sacando na conta do Pedro")
+//    contaPedro.saca(valor = 250.0)
+//    println(contaPedro.saldo)
+//
+//    println("saque em excesso na conta do Pedro")
+//    contaPedro.saca(valor = 250.0)
+//    println(contaPedro.saldo)
+//
+//    println("sacando na conta da Juliana")
+//    contaJuliana.saca(valor = 50.0)
+//    println(contaJuliana.saldo)
+//
+//    println("saque em excesso na conta da Juliana")
+//    contaJuliana.saca(valor = 500.0)
+//    println(contaJuliana.saldo)
+//
+//    println("transferência da conta da Juliana para conta do Pedro")
 
-    println("depósito realizado conta do Juliana")
-    println(contaJuliana.saldo)
-    contaJuliana.deposita(valor = 99.0)
-
-    println("sacando na conta do Pedro")
-    contaPedro.saca(valor = 250.0)
-    println(contaPedro.saldo)
-
-    println("saque em excesso na conta do Pedro")
-    contaPedro.saca(valor = 250.0)
-    println(contaPedro.saldo)
-
-    println("sacando na conta da Juliana")
-    contaJuliana.saca(valor = 50.0)
-    println(contaJuliana.saldo)
-
-    println("saque em excesso na conta da Juliana")
-    contaJuliana.saca(valor = 500.0)
-    println(contaJuliana.saldo)
-
-    println("transferência da conta da Juliana para conta do Pedro")
-
-}
-
-
-class Conta {
-    var titular = ""
-    var numero = 0
-    var saldo = 0.0
-
-    fun deposita(valor: Double) {
-        this.saldo += valor
-    }
-
-    fun saca(valor: Double) {
-        if (saldo >= valor) {
-            saldo -= valor
-        }
-    }
-
-    fun transfere(valor: Double, destino: Conta): Boolean {
-        if (saldo >= valor) {
-            saldo -= valor
-            destino.saldo += valor
-            return true
-        }
-        return false
-    }
-}
-
-fun testaReferencias() {
-    val numeroX = 10
-    var numeroY = numeroX
-    numeroY++
-
-    println("numeroX $numeroX")
-    println("numeroY $numeroY")
-
-    val contaJoao = Conta()
-    contaJoao.titular = "João"
-    var contaMaria = Conta()
-    contaMaria.titular = "Maria"
-    contaJoao.titular = "João"
-
-    println("titular conta João ${contaJoao.titular}")
-    println("titular conta Maria ${contaMaria.titular}")
-
-    println(contaJoao)
-    println(contaMaria)
-}
-
-fun testaLassos() {
-    val titular: String = "Pedro Lima"
-    val numeroConta: Int = 8000
-    var saldo = 8000
-
-    println("titular $titular")
-    println("numero da conta $numeroConta")
-    println("saldo da conta $saldo")
-
-}
-
-fun testaSaldo(saldo: Double) {
-    if (saldo > 0.0) {
-        println("conta é positiva")
-
+    if (contaJuliana.transfere(100.0, contaPedro)) {
+        println("transferência sucedida")
     } else {
-        if (saldo == 0.0) {
-            println("conta é neutra")
-        } else {
-            println("conta é negativa")
-        }
+        println("falha na tranferência")
+
     }
+
+    println(contaPedro.saldo)
+    println(contaJuliana.saldo)
+
+
 }
+
+//    fun testaReferencias() {
+//        val numeroX = 10
+//        var numeroY = numeroX
+//        numeroY++
+//
+//        println("numeroX $numeroX")
+//        println("numeroY $numeroY")
+//
+//        val contaJoao = Conta()
+//        contaJoao.titular = "João"
+//        var contaMaria = Conta()
+//        contaMaria.titular = "Maria"
+//        contaJoao.titular = "João"
+//
+//        println("titular conta João ${contaJoao.titular}")
+//        println("titular conta Maria ${contaMaria.titular}")
+//
+//        println(contaJoao)
+//        println(contaMaria)
+//    }
+
+//    fun testaLassos() {
+//        val titular: String = "Pedro Lima"
+//        val numeroConta: Int = 8000
+//        var saldo = 8000
+//
+//        println("titular $titular")
+//        println("numero da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//
+//    }
+
+//    fun testaSaldo(saldo: Double) {
+//        if (saldo > 0.0) {
+//            println("conta é positiva")
+//
+//        } else {
+//            if (saldo == 0.0) {
+//                println("conta é neutra")
+//            } else {
+//                println("conta é negativa")
+//            }
+//        }
+//    }
